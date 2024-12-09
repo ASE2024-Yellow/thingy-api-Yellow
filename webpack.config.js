c369716018d67985d7f9a9e52753bceb1c087ef1
daz <william.anze.dan@gmail.com>
2024-12-09 14:22:04 +0100

    fix: updated webpack.config.js and webpack.yml

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,22 @@
-module.exports = {
-  entry: './server.ts'
+import { resolve as _resolve } from 'path';
+
+export const entry = './server.ts';
+export const target = 'node';
+export const mode = 'production';
+export const module = {
+  rules: [
+    {
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    },
+  ],
 };
+export const resolve = {
+  extensions: ['.ts', '.js'],
+};
+export const output = {
+  filename: 'server.js',
+  path: _resolve(__dirname, 'dist'),
+};
+
